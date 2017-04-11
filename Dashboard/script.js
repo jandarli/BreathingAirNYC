da170313535770f93d7163e186c6daf9a354be1a
janicedarling <janicedarling1@gmail.com>
2017-04-11 18:37:28 -0400

    Adding Leaflet map

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,8 +1,11 @@
-var map = new google.maps.Map(d3.select("#map").node(), {
-  zoom: 8,
-  center: new google.maps.LatLng(40.730610
-,-73.935242),
-  mapTypeId: google.maps.MapTypeId.TERRAIN
-});
+// initialize the map
+var map = L.map('map').setView([40.730610,-73.935242], 12);
 
+// load a tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+{
+  // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 17,
+    minZoom: 9
+}).addTo(map);
 
