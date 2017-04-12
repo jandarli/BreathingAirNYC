e409960817ddb5696468c3853630aa8153514e45
janicedarling <janicedarling1@gmail.com>
2017-04-11 20:20:46 -0400

    Adding Mapbox layer

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,10 +1,14 @@
+//Create account on Mapbox for access token or email for accessToken
+accessToken = ''
+
 // initialize the map
-var map = L.map('map').setView([40.730610,-73.935242], 12);
+ var map = L.map('map').setView([40.730610,-73.935242], 12);
 
 // load a tile layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+L.tileLayer('https://api.mapbox.com/styles/v1/jdarlin000/cj1e62kle002d2rtc41j3rkbs/tiles/256/{z}/{x}/{y}?access_token=' + accessToken,
 {
   // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    accessToken: accessToken,
     maxZoom: 17,
     minZoom: 9
 }).addTo(map);
